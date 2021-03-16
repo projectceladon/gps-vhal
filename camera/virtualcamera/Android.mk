72973352daa4c300aa4af00649f31139734d73f3
Shakthi Prashanth M <shakthi.prashanth.m@intel.com>
2021-03-16 23:26:19 +0530

    Refactor and Restructure
    - Source code folder resture
    - Removed unnecessary files such as RemoteCamera, RemoteClient, Emulated
      Camera modules, etc. They'readded on need basis in future.
    - And many other clean refactorings in source code.
    
    Signed-off-by: Shakthi Prashanth M <shakthi.prashanth.m@intel.com>

--- camera/virtualcamera/Android.mk
+++ camera/virtualcamera/Android.mk
@@ -12,25 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#ifeq ($(TARGET_USE_CAMERA_VHAL), true)
+ifeq ($(TARGET_USE_CAMERA_VHAL), true)
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
 LOCAL_MULTILIB := 64
 
-# Emulator camera module########################################################
-
-emulator_camera_module_relative_path := hw
-emulator_camera_cflags := -fno-short-enums -DREMOTE_HARDWARE
-emulator_camera_cflags += -Wno-unused-parameter -Wno-missing-field-initializers
-emulator_camera_clang_flags := -Wno-c++11-narrowing -Werror -Wno-unknown-pragmas
+camera_vhal_module_relative_path := hw
+camera_vhal_cflags := -fno-short-enums -DREMOTE_HARDWARE
+camera_vhal_cflags += -Wno-unused-parameter -Wno-missing-field-initializers
+camera_vhal_clang_flags := -Wno-c++11-narrowing -Werror -Wno-unknown-pragmas
 
 ifeq ($(BOARD_USES_GRALLOC1), true)
-emulator_camera_cflags += -DUSE_GRALLOC1
+camera_vhal_cflags += -DUSE_GRALLOC1
 endif
 
-emulator_camera_shared_libraries := \
+camera_vhal_shared_libraries := \
     libbinder \
     libexif \
     liblog \
@@ -53,69 +51,52 @@ emulator_camera_shared_libraries := \
     libswresample \
     libswscale
 
-emulator_camera_static_libraries := \
+camera_vhal_static_libraries := \
 	android.hardware.camera.common@1.0-helper \
 	libyuv_static
 
-emulator_camera_c_includes := external/libjpeg-turbo \
+camera_vhal_c_includes := external/libjpeg-turbo \
 	external/libexif \
 	external/libyuv/files/include \
 	frameworks/native/include/media/hardware \
 	device/generic/goldfish/include \
 	device/generic/goldfish-opengl/system/OpenglSystemCommon \
 	hardware/libhardware/modules/gralloc \
-	$(LOCAL_PATH)/cg-codec/include \
-	$(LOCAL_PATH)/cg-codec/prebuilts/ffmpeg-4.1.5/android-x86_64/include \
+	$(LOCAL_PATH)/include \
+	$(LOCAL_PATH)/prebuilts/ffmpeg-4.1.5/android-x86_64/include \
 	$(call include-path-for, camera)
 
-emulator_camera_src := \
-	VirtualCameraHal.cpp \
-	VirtualCameraFactory.cpp \
-	VirtualBaseCamera.cpp \
-	VirtualCamera.cpp \
-	VirtualCameraDevice.cpp \
-	VirtualRemoteCamera.cpp \
-	VirtualRemoteCameraDevice.cpp \
-	VirtualFakeCamera.cpp \
-	VirtualFakeCameraDevice.cpp \
-	VirtualFakeRotatingCameraDevice.cpp \
-	Converters.cpp \
-	PreviewWindow.cpp \
-	CallbackNotifier.cpp \
-	RemoteClient.cpp \
-	JpegCompressor.cpp \
-	VirtualCamera2.cpp \
-	VirtualFakeCamera2.cpp \
-	VirtualRemoteCamera2.cpp \
-	fake-pipeline2/Scene.cpp \
-	fake-pipeline2/Sensor.cpp \
-	fake-pipeline2/JpegCompressor.cpp \
-	VirtualCamera3.cpp \
-	VirtualFakeCamera3.cpp \
-	VirtualRemoteCamera3.cpp \
-	remote-pipeline3/RemoteSensor.cpp \
-	Exif.cpp \
-	Thumbnail.cpp \
-	WorkerThread.cpp \
-	CameraSocketServerThread.cpp \
-	CameraSocketCommand.cpp \
-	cg-codec/src/cg_codec.cpp \
-	cg-codec/src/cg_protocol.cpp
+camera_vhal_src := \
+	src/VirtualCameraHal.cpp \
+	src/VirtualCameraFactory.cpp \
+	src/VirtualBaseCamera.cpp \
+	src/Converters.cpp \
+	src/NV21JpegCompressor.cpp \
+	src/fake-pipeline2/Scene.cpp \
+	src/fake-pipeline2/Sensor.cpp \
+	src/fake-pipeline2/JpegCompressor.cpp \
+	src/VirtualCamera3.cpp \
+	src/VirtualFakeCamera3.cpp \
+	src/Exif.cpp \
+	src/Thumbnail.cpp \
+	src/CameraSocketServerThread.cpp \
+	src/CameraSocketCommand.cpp \
+	src/CGCodec.cpp
 
 LOCAL_VENDOR_MODULE := true
-LOCAL_MODULE_RELATIVE_PATH := ${emulator_camera_module_relative_path}
-LOCAL_CFLAGS := ${emulator_camera_cflags}
+LOCAL_MODULE_RELATIVE_PATH := ${camera_vhal_module_relative_path}
+LOCAL_CFLAGS := ${camera_vhal_cflags}
 LOCAL_CPPFLAGS += -std=c++17
-LOCAL_CLANG_CFLAGS += ${emulator_camera_clang_flags}
+LOCAL_CLANG_CFLAGS += ${camera_vhal_clang_flags}
 
-LOCAL_SHARED_LIBRARIES := ${emulator_camera_shared_libraries}
-LOCAL_STATIC_LIBRARIES := ${emulator_camera_static_libraries}
-LOCAL_C_INCLUDES += ${emulator_camera_c_includes}
-LOCAL_SRC_FILES := ${emulator_camera_src}
+LOCAL_SHARED_LIBRARIES := ${camera_vhal_shared_libraries}
+LOCAL_STATIC_LIBRARIES := ${camera_vhal_static_libraries}
+LOCAL_C_INCLUDES += ${camera_vhal_c_includes}
+LOCAL_SRC_FILES := ${camera_vhal_src}
 
 LOCAL_EXPORT_C_INCLUDES := \
-	$(LOCAL_PATH)/cg-codec/include \
-	$(LOCAL_PATH)/cg-codec/prebuilts/ffmpeg-4.1.5/android-x86_64/include \
+	$(LOCAL_PATH)/include \
+	$(LOCAL_PATH)/prebuilts/ffmpeg-4.1.5/android-x86_64/include \
 
 # to support platfrom build system
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_EXPORT_C_INCLUDES)
@@ -125,5 +106,5 @@ LOCAL_MODULE := camera.$(TARGET_PRODUCT)
 include $(BUILD_SHARED_LIBRARY)
 
 # Build all subdirectories #####################################################
-#include $(call all-makefiles-under,$(LOCAL_PATH))
-#endif # TARGET_USE_CAMERA_VHAL
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif # TARGET_USE_CAMERA_VHAL
